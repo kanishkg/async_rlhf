3226624bf5aaa111a6169e8871d09e643e770163
Kanishk Gandhi <kanishk050296@gmail.com>
2024-11-27 14:51:57 -0800

    debug

--- test_vllm.py
+++ test_vllm.py
@@ -17,6 +17,15 @@ from accelerate import Accelerator
 accelerator = Accelerator()
 if accelerator.is_main_process:
     print("Hello from main process")
+    llm = LLM(
+                model="meta-llama/Llama-3.1-8B-Instruct",
+                max_num_seqs=16,
+                swap_space=64,
+                dtype="bfloat16",
+                max_model_len=2048,
+                tensor_parallel_size=1,
+                device="cuda:3",
+    )
 else:
     print("Hello from subprocess")
 
