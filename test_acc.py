90be3655a1855479fafac057850edcf64608d28e
Kanishk Gandhi <kanishk050296@gmail.com>
2024-11-28 14:51:20 -0800

    debug

---
+++ test_acc.py
@@ -0,0 +1,78 @@
+import multiprocessing
+import threading
+import torch
+import os
+from dataclasses import dataclass, field
+from typing import Optional
+import requests
+
+from datasets import load_dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+)
+
+from vllm import LLM
+from src.vllm_utils import vllm_single_gpu_patch
+import sglang as sgl
+from sglang.utils import (
+    execute_shell_command,
+    wait_for_server,
+    terminate_process,
+    print_highlight,
+)
+from accelerate import Accelerator
+
+def vllm_generate(model_name_or_path: str, vllm_device: str, vllm_dtype: str, vllm_gpu_memory_utilization: float):
+    llm = LLM(
+        model=model_name_or_path,
+        revision="main",
+        tokenizer_revision="main",
+        tensor_parallel_size=1,
+        device=vllm_device,
+        dtype=vllm_dtype,
+        gpu_memory_utilization=vllm_gpu_memory_utilization,
+    )
+    print(f"🔥🔥🔥 vllm loaded")
+    print(f"🔥🔥🔥 vllm loaded in {vllm_dtype}")
+    llmp = llm.llm_engine.model_executor.driver_worker.model_runner.model
+
+accelerator = Accelerator()
+if accelerator.is_main_process:
+    print("Hello from main process")
+    # server_process = execute_shell_command(f"python -m sglang.launch_server --model-path meta-llama/Llama-3.1-8B-Instruct --port=30010 --device=cuda --dtype=bfloat16 --max-total-tokens=2048 --base-gpu-id=3")
+    wait_for_server("http://localhost:30010")
+    print("Server is ready")
+    sampling_params = {"temperature": 1.0, "top_p": 0.95, "max_new_tokens": 2048}
+    data = {"text": ["What is the capital of France?"]*256, "sampling_params": sampling_params}
+    response = requests.post("http://localhost:30010/generate", json=data)
+    print(len(response.json()))
+    print(response.json()["text"])
+    print(response.json()["meta_info"])
+    # meta_info = response.json()["meta_info"]["output_token_logprobs"]
+    # logprobs = [mi[0] for mi in meta_info]
+    # token_ids = [mi[1] for mi in meta_info]
+    # print(logprobs)
+    # print(token_ids)
+    # tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B-Instruct")
+    # print(tokenizer.decode(token_ids))
+
+
+    # vllm_single_gpu_patch()
+    # thread = threading.Thread(
+    #             target=vllm_generate,
+    #             args=(
+    #                 "meta-llama/Llama-3.1-8B-Instruct",
+    #                 "cuda:3",
+    #                 "bfloat16",
+    #                 0.95,
+    #             ),
+    #         )
+    # thread.start()
+
+else:
+    print("Hello from subprocess")
+
+accelerator.wait_for_everyone()
+print("All processes are ready")
--- test_vllm.py
+++ test_vllm.py
@@ -1,3 +1,4 @@
+import queue
 import multiprocessing
 import threading
 import torch
@@ -5,6 +6,7 @@ import os
 from dataclasses import dataclass, field
 from typing import Optional
 import requests
+import time
 
 from datasets import load_dataset
 from transformers import (
@@ -22,13 +24,11 @@ from sglang.utils import (
     terminate_process,
     print_highlight,
 )
-from accelerate import Accelerator
 
-def vllm_generate(model_name_or_path: str, vllm_device: str, vllm_dtype: str, vllm_gpu_memory_utilization: float):
+def vllm_generate(model_name_or_path: str, vllm_device: str, vllm_dtype: str, vllm_gpu_memory_utilization: float, param_prompt_Q: queue.Queue):
     llm = LLM(
         model=model_name_or_path,
-        revision="main",
-        tokenizer_revision="main",
+        enforce_eager=True,
         tensor_parallel_size=1,
         device=vllm_device,
         dtype=vllm_dtype,
@@ -38,41 +38,38 @@ def vllm_generate(model_name_or_path: str, vllm_device: str, vllm_dtype: str, vl
     print(f"🔥🔥🔥 vllm loaded in {vllm_dtype}")
     llmp = llm.llm_engine.model_executor.driver_worker.model_runner.model
 
-accelerator = Accelerator()
-if accelerator.is_main_process:
-    print("Hello from main process")
-    # server_process = execute_shell_command(f"python -m sglang.launch_server --model-path meta-llama/Llama-3.1-8B-Instruct --port=30010 --device=cuda --dtype=bfloat16 --max-total-tokens=2048 --base-gpu-id=3")
-    wait_for_server("http://localhost:30010")
-    print("Server is ready")
-    sampling_params = {"temperature": 1.0, "top_p": 0.95, "max_new_tokens": 2048}
-    data = {"text": ["What is the capital of France?"]*256, "sampling_params": sampling_params}
-    response = requests.post("http://localhost:30010/generate", json=data)
-    print(len(response.json()))
-    print(response.json()["text"])
-    print(response.json()["meta_info"])
-    # meta_info = response.json()["meta_info"]["output_token_logprobs"]
-    # logprobs = [mi[0] for mi in meta_info]
-    # token_ids = [mi[1] for mi in meta_info]
-    # print(logprobs)
-    # print(token_ids)
-    # tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B-Instruct")
-    # print(tokenizer.decode(token_ids))
+    while True:
+        i += 1
+        model_named_parameters = param_prompt_Q.get()
+        if i > 2:
+            vllm_start_time = time.time()
+            print("🔥🔥🔥 Loading weights using shared memory;" "we expect the generations to be completely different")
+            llmp.load_weights(model_named_parameters)
+            print(f"load weights took: {time.time() - vllm_start_time:.2f} seconds")
+        time.sleep(5)  # Check every 10 seconds
 
 
-    # vllm_single_gpu_patch()
-    # thread = threading.Thread(
-    #             target=vllm_generate,
-    #             args=(
-    #                 "meta-llama/Llama-3.1-8B-Instruct",
-    #                 "cuda:3",
-    #                 "bfloat16",
-    #                 0.95,
-    #             ),
-    #         )
-    # thread.start()
 
-else:
-    print("Hello from subprocess")
+def main():
+    vllm_single_gpu_patch()
+    param_prompt_Q = queue.Queue(maxsize=1)
+    thread = threading.Thread(
+                target=vllm_generate,
+                args=(
+                    "/scr/kanishkg/rloo_temp",
+                    "cuda:0",
+                    "bfloat16",
+                    0.95,
+                ),
+            )
+    thread.start()
 
-accelerator.wait_for_everyone()
-print("All processes are ready")
+    time.sleep(5)
+    model = AutoModelForCausalLM.from_pretrained("/scr/kanishkg/rloo_temp")
+    print("saving model")
+    model.save_pretrained("/scr/kanishkg/rloo_temp")
+    param_prompt_Q.put(model.named_parameters.items())
+    print("model saved")
+
+if __name__ == "__main__":
+    main()
