584df37808b137391fb9a500a8dae734a6bdc7bb
Kanishk Gandhi <kanishk050296@gmail.com>
2024-11-28 15:15:57 -0800

    debug

--- test_vllm.py
+++ test_vllm.py
@@ -15,7 +15,7 @@ from transformers import (
     AutoTokenizer,
 )
 
-from vllm import LLM
+from vllm import LLM, SamplingParams
 from src.vllm_utils import vllm_single_gpu_patch
 import sglang as sgl
 from sglang.utils import (
@@ -25,7 +25,7 @@ from sglang.utils import (
     print_highlight,
 )
 
-def vllm_generate(model_name_or_path: str, vllm_device: str, vllm_dtype: str, vllm_gpu_memory_utilization: float, param_prompt_Q: queue.Queue):
+def vllm_generate(model_name_or_path: str, vllm_device: str, vllm_dtype: str, vllm_gpu_memory_utilization: float, param_prompt_Q: queue.Queue, response_ids_Q: queue.Queue):
     llm = LLM(
         model=model_name_or_path,
         enforce_eager=True,
@@ -37,22 +37,32 @@ def vllm_generate(model_name_or_path: str, vllm_device: str, vllm_dtype: str, vl
     print(f"🔥🔥🔥 vllm loaded")
     print(f"🔥🔥🔥 vllm loaded in {vllm_dtype}")
     llmp = llm.llm_engine.model_executor.driver_worker.model_runner.model
+    sampling_params = SamplingParams(
+        n=1,
+        temperature=1,
+        max_tokens=2048
+    )
 
     i = 0
     while True:
         i += 1
         print(f"🔥🔥🔥 Waiting for weights to be loaded")
-        model_named_parameters = param_prompt_Q.get()
+        model_named_parameters, queries_list = param_prompt_Q.get()
         print(f"🔥🔥🔥 Weights are loaded")
-        if i > 2:
+        if i > 0:
             vllm_start_time = time.time()
             print("🔥🔥🔥 Loading weights using shared memory;" "we expect the generations to be completely different")
+
             llmp.load_weights(model_named_parameters)
             print(f"load weights took: {time.time() - vllm_start_time:.2f} seconds")
+        outputs = llm.generate(prompt_token_ids=queries_list, sampling_params=sampling_params, use_tqdm=False)
+        responses = [output[0].text for output in outputs]
+        response_ids_Q.put(responses)
 
 def main():
     vllm_single_gpu_patch()
     model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.1-8B-Instruct")
+    response_ids_Q = queue.Queue(maxsize=1)
     param_prompt_Q = queue.Queue(maxsize=1)
     thread = threading.Thread(
                 target=vllm_generate,
@@ -61,16 +71,23 @@ def main():
                     "cuda:0",
                     "bfloat16",
                     0.95,
-                    param_prompt_Q
+                    param_prompt_Q,
+                    response_ids_Q,
                 ),
             )
     thread.start()
-
+    prompts = [
+        "Hello, my name is",
+        "The president of the United States is",
+        "The capital of France is",
+        "The future of AI is",
+    ]
+    
     print("🔥🔥🔥 Putting weights in memory")
-    param_prompt_Q.put(model.named_parameters())
-    param_prompt_Q.put(model.named_parameters())
-    param_prompt_Q.put(model.named_parameters())
+    param_prompt_Q.put((model.named_parameters(), prompts))
     print("🔥🔥🔥 Weights are in memory")
+    respones = response_ids_Q.get()
+    print(respones)
 
 if __name__ == "__main__":
     main()
