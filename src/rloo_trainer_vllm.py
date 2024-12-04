1b4923125dfa54d245ceb212aa51e356059b3659
Kanishk Gandhi <kanishk050296@gmail.com>
2024-12-04 12:22:03 -0800

    debug

--- src/rloo_trainer_vllm.py
+++ src/rloo_trainer_vllm.py
@@ -215,7 +215,7 @@ class RLOOTrainer(Trainer):
                     self.reward_model, args.per_device_train_batch_size, config.fp16, config.bf16
                 )
             self.ref_policy = prepare_deepspeed(
-                self.ref_policy, args.per_device_train_batch_size, config.fp16, config.bf16
+                self.ref_policy, args.per_device_train_batch_size, fp16=False, bf16=True 
             )
         else:
             if self.reward_model is not None:
@@ -429,7 +429,7 @@ class RLOOTrainer(Trainer):
                     with torch.no_grad():
                         # NOTE (kg): forward pass might be slow, as pass attentions mask, pos ids. torch compile cant handle this
                         # NOTE (kg): we should change it to use model() instead of forward()
-                        ref_output = forward(model, query_response, tokenizer.pad_token_id)
+                        ref_output = forward(ref_policy, query_response, tokenizer.pad_token_id)
                         ref_logits = ref_output.logits[:, context_length - 1 : -1]
                         # ref_logits /= args.temperature + 1e-7
                         ref_all_logprob = F.log_softmax(ref_logits, dim=-1)
