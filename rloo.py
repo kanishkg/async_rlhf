2b625fee0fb36d1b820a447b374e218fd0afb019
Kanishk Gandhi <kanishk050296@gmail.com>
2024-12-04 13:31:33 -0800

    debug

--- rloo.py
+++ rloo.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
         import datetime
         wandb.init(project="rloo",
                    name="rloo_countdown",
-                   id=f"rloo_countdown_{datetime.now().strftime('%Y%m%d_%H%M%S')}",
+                   id=f"rloo_countdown_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}",
                    entity="cocolab")
 
 
